5bd4c0ba36bb7275a2bd8effdb228caa7be05678
ravilushqa <Vfvfgfgf777>
2017-10-10 20:08:45 +0300

    Meta Details and Pagination

--- app/Thread.php
+++ app/Thread.php
@@ -8,33 +8,62 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
 
+        static::addGlobalScope('replyCount', function ($builder) {
+            $builder->withCount('replies');
+        });
+    }
+
+
+    /**
+     * @return string
+     */
     public function path()
     {
         return env('APP_URL') . '/threads/' . $this->channel->slug . '/' . $this->id;
 //        return route('threads.show', $this->getKey());
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    /**
+     * @param $reply
+     */
     public function addReply($reply)
     {
         $this->replies()->create($reply);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function channel()
     {
         return $this->belongsTo(Channel::class);
     }
 
+    /**
+     * @param $query
+     * @param $filters
+     * @return mixed
+     */
     public function scopeFilter($query, $filters)
     {
         return $filters->apply($query);

7443b99b18e43c487867315d59718cd397d372e1
ravilushqa <Vfvfgfgf777>
2017-10-21 22:59:47 +0300

    Flash Messaging With Vue

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,17 +37,8 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+window.events = new Vue();
 
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.flash = function (message) {
+    window.events.$emit('flash', message);
+};
